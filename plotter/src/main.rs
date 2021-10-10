19682a141ec7c3a413eb78016a8f9dfb35366393
Reuben Staley <lighthousemaniac@gmail.com>
2021-10-10 23:50:27 +0000

    oct 10

--- plotter/src/main.rs
+++ plotter/src/main.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 use std::env::args;
 use std::fs::{self, File};
-use std::io::BufReader;
+use std::io::{BufReader, Write};
 
 use chrono::{
-    naive::{NaiveDate, NaiveTime},
+    naive::{MIN_DATE, NaiveDate, NaiveTime},
     Utc,
     Duration,
     Datelike,
@@ -21,6 +21,7 @@ use assetlib::{
 struct EntHist {
     active: bool,
     registered: bool,
+    notable: bool,
     history: Vec<(NaiveDate, u32)>,
 }
 
@@ -29,6 +30,7 @@ impl EntHist {
         EntHist {
             active: true,
             registered: true,
+            notable: false,
             history: Vec::new(),
         }
     }
@@ -59,6 +61,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         panic!("unknown number of args: {}", args.len())
     };
 
+    let mut start_date = MIN_DATE;
+
     let mut context = Context::from_datetime(end_date);
 
     let mut coinhist = HashMap::new();
@@ -69,6 +73,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     while let Some(d) = gsdparser.next_raw() {
         match d {
+            Directive::Date(d) => {
+                start_date = d;
+            }
             Directive::Entity(ref e) => {
                 coinhist.entry(e.identifier().clone()).or_insert(EntHist::new());
             }
@@ -122,10 +129,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     if !hist.registered {
                         continue;
                     }
+                    
+                    let balance = context.entities().get(e).unwrap().balance(Currency::Coin);
+
+                    if balance > 1800 {
+                        hist.notable = true;
+                    }
 
                     hist.history.push((
-                        d + Duration::days(1),
-                        context.entities().get(e).unwrap().balance(Currency::Coin)
+                        d, //+ Duration::days(1),
+                        balance
                     ));
                 }
             }
@@ -143,24 +156,59 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
 
             hist.history.push((
-                d + Duration::days(1),
+                d, // + Duration::days(1),
                 context.entities().get(e).unwrap().balance(Currency::Coin)
             ));
         }
     }
 
-    let root = BitMapBackend::new("chart.png", (1200, 800)).into_drawing_area();
+    // ---UNCOMMENT FOR CSV GENERATION--- //
+    //let mut table = Vec::new();
+
+    //let mut hists = coinhist.iter().map(|(e, hist)| {
+    //    let mut v = Vec::new();
+    //    v.push(e.clone());
+    //    table.push(v);
+    //    hist.history.iter().peekable()
+    //}).collect::<Vec<_>>();
+
+    //for d in start_date.iter_days() {
+    //    if d >= end_date.date() {
+    //        break;
+    //    }
+
+    //    for (i, col) in table.iter_mut().enumerate() {
+    //        let hist = &mut hists[i];
+    //        if let Some((hd, entry)) = hist.peek() {
+    //            if d != *hd {
+    //                col.push("".to_string());
+    //            } else {
+    //                col.push(entry.to_string());
+    //                hist.next();
+    //            }
+    //        }
+    //    }
+    //}
+
+    //let table_out = table.iter().map(|c| c.join(",")).fold(String::new(), |acc, item| {
+    //    acc + &item[..] + "\n"
+    //});
+
+    //let mut f = File::create("table.txt")?;
+    //f.write_all(table_out.as_bytes())?;
+
+    let root = BitMapBackend::new("chart.png", (1350, 800)).into_drawing_area();
     root.fill(&RGBColor(180, 180, 180))?;
 
     let mut chart = ChartBuilder::on(&root)
-        .caption("Coins over time for currently active players", ("sans serif", 50).into_font())
+        .caption("Coins over time for entities who at one time owned over 1800 coins", ("sans serif", 50).into_font())
         .margin(10)
         .x_label_area_size(30)
         .y_label_area_size(40)
         .build_cartesian_2d(
-            (NaiveDate::from_ymd(2021, 1, 18)..end_date.date() + Duration::days(20))
+            (NaiveDate::from_ymd(2021, 1, 18)..end_date.date() + Duration::days(30))
                 .monthly(),
-            0u32..7500u32
+            0u32..13000u32
         )?;
 
     chart.configure_mesh()
@@ -169,7 +217,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut sorted = coinhist.iter()
         .filter(|(_, h)| {
-            h.active && h.registered
+            /* h.active && h.registered && */ h.notable
         })
         .filter(|(_, h)| {
             h.history.iter()
@@ -177,9 +225,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .next()
                 .is_some()
         })
-        .filter(|(e, _)| {
-            context.entities().get(e).unwrap().kind().is_player()
-        })
+        //.filter(|(e, _)| {
+        //    context.entities().get(e).unwrap().kind().is_player()
+        //})
         .collect::<Vec<_>>();
 
     sorted.sort_by(|(e1, _), (e2, _)| {
